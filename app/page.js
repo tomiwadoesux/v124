06a5a4351ebc4115118cefbe9beae91b04b378d1
tomiwadoesux <141045581+tomiwadoesux@users.noreply.github.com>
2025-12-10 14:54:21 -0600

    upd

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
-import Body from "@/components/Body";
-import DitheredGradientPage from "@/components/DitherBg";
+import Body from "../components/Body";
 import Image from "next/image";
 
 export default function Home() {
